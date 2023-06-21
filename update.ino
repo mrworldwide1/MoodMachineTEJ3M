66ca46e3cb47dbed7868ca75d77d5d8c77a2adeb
Lucas Leung <66652566+mrworldwide1@users.noreply.github.com>
2023-06-21 15:10:44 -0400

    Update update.ino

--- update.ino
+++ update.ino
@@ -1,5 +1,5 @@
-// BUTTON DATA PINS - INPUTS
-const int  buttonGreenPin = 2;    // green button
+// BUTTON DATA PINS - INPUT
+const int buttonGreenPin = 2;    // green button
 const int buttonRedPin = 3;       // red button
 const int buttonYellowPin = 4;    // yellow button
 
@@ -8,9 +8,7 @@ const int ledGreenPin = 8;       // green led
 const int ledRedPin = 10;         // red led
 const int ledYellowPin = 9;     // yellow led
 
-// Variables will change:
-int buttonPushCounter = 0;   // counter for the number of button presses
-// IMPORTANT: Counter for current mood, increment/decrement - just added, june 21 2023
+// Counter for current mood, increment/decrement
 int counter = 0;
 
 // GREEN BUTTON STATE CHECK
@@ -31,49 +29,48 @@ void setup() {
   pinMode(buttonRedPin, INPUT);
   pinMode(buttonYellowPin, INPUT);
  
-  // initialize the LED as an output:
+  // initialize the LED as an output
   pinMode(ledGreenPin, OUTPUT);
   pinMode(ledRedPin, OUTPUT);
   pinMode(ledYellowPin, OUTPUT);
  
-  // initialize serial communication:
+  // initialize serial communication
   Serial.begin(9600);
 }
 
 
 void loop() {
-  // read the pushbutton input pin:
+  // read the pushbutton input pin
   buttonStateGreen = digitalRead(buttonGreenPin);
   buttonStateRed = digitalRead(buttonRedPin);
   buttonStateYellow = digitalRead(buttonYellowPin);
 
   // seperate into scopes using {}
-
+  
   // scope block for green
   {
-      // compare the buttonStates to the previous states
+    // compare the buttonStates to the previous states
     if ((buttonStateGreen != lastButtonStateGreen)) {
       // if the state has changed, increment the counter
       if (buttonStateGreen == HIGH) {
         // if the current state is HIGH then the button went from off to on:
-        greenOn(); // turn on green led (see respective defined function)
-        counter+=1;
+        greenLEDOn(); // turn on green led (see respective defined function)
+        counter += 1;
       }
-      // Delay a little bit to avoid bouncing
-      delay(50);
+      delay(50); // Delay a little bit to avoid bouncing
     }
     // save the current state as the last state, for next time through the loop
     lastButtonStateGreen = buttonStateGreen;
   }
 
-  // // scope block for red
+  // scope block for yellow
   {
-      // compare the buttonStates to the previous states
+    // compare the buttonStates to the previous states
     if ((buttonStateYellow != lastButtonStateYellow)) {
       // if the state has changed, increment the counter
       if (buttonStateYellow == HIGH) {
         // if the current state is HIGH then the button went from off to on:
-        yellowOn(); // turns yellow led on (see the respective defined function)
+        yellowLEDOn(); // turns yellow led on (see the respective defined function)
         //counter+=0; don't need this because yellow literally does nothing
       }
       delay(50);
@@ -82,48 +79,50 @@ void loop() {
     lastButtonStateYellow = buttonStateYellow;
   }
 
-  //scope for red
+  //scope block for red
   {
-      // compare the buttonStates to the previous states
+    // compare the buttonStates to the previous states
     if ((buttonStateRed != lastButtonStateRed)) {
       // if the state has changed, increment the counter
       if (buttonStateRed == HIGH) {
         // if the current state is HIGH then the button went from off to on:
-        redOn(); // turns red led on (see respective defined function)
-        counter-=1;
+        redLEDOn(); // turns red led on (see respective defined function)
+        counter -= 1;
       }
       delay(50);
     }
     // save the current state as the last state, for next time through the loop
     lastButtonStateRed = buttonStateRed;
-  
   }
 
-  Serial.println(counter);
-
+  // Determine which LED should be lit based on mood counter
   if (counter > 0) {
-    greenOn();
+    greenLEDOn();
   } else if (counter < 0) {
-    redOn();
+    redLEDOn();
   } else {
-    yellowOn();
+    yellowLEDOn();
   }
+    Serial.println(counter); // debug print current counter
 }
 
-void redOn() {
+// turn on red LED
+void redLEDOn() {
   digitalWrite(ledGreenPin, LOW);
   digitalWrite(ledYellowPin, LOW);
-  digitalWrite(ledRedPin, HIGH); // turn on led
+  digitalWrite(ledRedPin, HIGH); // turn on red led
 }
 
-void greenOn() {
+// turn on green LED
+void greenLEDOn() {
   digitalWrite(ledRedPin, LOW);
   digitalWrite(ledYellowPin, LOW);
-  digitalWrite(ledGreenPin, HIGH); // turn on led
+  digitalWrite(ledGreenPin, HIGH);
 }
 
-void yellowOn() {
+// turn on yellow LED
+void yellowLEDOn() {
   digitalWrite(ledGreenPin, LOW);
   digitalWrite(ledRedPin, LOW);
-  digitalWrite(ledYellowPin, HIGH); // turn on led
+  digitalWrite(ledYellowPin, HIGH);
 }
